329aa9b3b565a1c0d6eb784ff76d428bb97af271
ligi <ligi@ligi.de>
2011-12-03 15:41:26 +0100

    misc

---
+++ src/org/rhok/offlike/FakeTrigger.java
@@ -0,0 +1,39 @@
+package org.rhok.offlike;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class FakeTrigger extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    	LinearLayout lin=new LinearLayout(this);
+        
+        for ( int i=0;i<5;i++) {
+        	Button btn=new Button(this);
+        	btn.setText("campaign " + i);
+        	btn.setTag(i);
+        	btn.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					Intent intent= new Intent();
+					Activity a=FakeTrigger.this;
+					intent.setAction(Intent.ACTION_VIEW);
+					intent.setData(Uri.parse("http://offlike.herokuapp.com/campaign/"+(Integer)v.getTag()));
+					a.startActivity(intent);
+				}
+        		
+        	});
+        	lin.addView(btn);
+        }
+        this.setContentView(lin);
+    }
+}
--- src/org/rhok/offlike/OfflikeActivity.java
+++ src/org/rhok/offlike/OfflikeActivity.java
@@ -1,10 +1,18 @@
 package org.rhok.offlike;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class OfflikeActivity extends Activity {
+	
+	public final static int REQUEST_ID_SCAN=0;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,4 +27,52 @@ public class OfflikeActivity extends Activity {
         this.setContentView(tv);
         
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		this.getMenuInflater().inflate(R.menu.default_mainview, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_fake:
+			Intent i=new Intent(this,FakeTrigger.class);
+			this.startActivity(i);
+			break;
+			
+        case android.R.id.home:
+            // app icon in action bar clicked; go home
+            Intent intent = new Intent(this, OfflikeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+            
+        case R.id.menu_scan:
+            Intent scan_intent = new Intent("com.google.zxing.client.android.SCAN");
+            scan_intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(scan_intent, REQUEST_ID_SCAN);
+            break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	    if (requestCode == REQUEST_ID_SCAN) {
+	        if (resultCode == RESULT_OK) {
+	            String contents = intent.getStringExtra("SCAN_RESULT");
+	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	            Log.i("","contents" + contents);
+	            Intent i=new Intent(this,OfflikeActivity.class);
+	            i.setData(Uri.parse(contents));
+	            this.startActivity(i);
+	            // Handle successful scan
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // Handle cancel
+	        }
+	    }
+	}
+
 }
