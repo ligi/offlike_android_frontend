83b75f7acd0308f647034c3e0f3660b4ead21503
ligi <ligi@ligi.de>
2011-12-03 18:37:06 +0100

    move some functions to the base activity

--- src/org/rhok/offlike/OfflikeActivity.java
+++ src/org/rhok/offlike/OfflikeActivity.java
@@ -1,17 +1,14 @@
 package org.rhok.offlike;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
-import android.util.Log;
 import android.widget.TextView;
 
 public class OfflikeActivity extends OfflikeFragmentActivity {
 	
-	public final static int REQUEST_ID_SCAN=0;
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,37 +45,11 @@ public class OfflikeActivity extends OfflikeFragmentActivity {
 			this.startActivity(i_pend);
 			break;
 			
-        case android.R.id.home:
-            // app icon in action bar clicked; go home
-            Intent intent = new Intent(this, OfflikeActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            return true;
-            
-        case R.id.menu_scan:
-            Intent scan_intent = new Intent("com.google.zxing.client.android.SCAN");
-            scan_intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-            startActivityForResult(scan_intent, REQUEST_ID_SCAN);
-            break;
+        
 		}
 		return super.onOptionsItemSelected(item);
 	}
 	
-	
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-	    if (requestCode == REQUEST_ID_SCAN) {
-	        if (resultCode == RESULT_OK) {
-	            String contents = intent.getStringExtra("SCAN_RESULT");
-	            //String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-	            Log.i("","contents" + contents);
-	            Intent i=new Intent(this,OfflikeActivity.class);
-	            i.setData(Uri.parse(contents));
-	            this.startActivity(i);
-	            // Handle successful scan
-	        } else if (resultCode == RESULT_CANCELED) {
-	            // Handle cancel
-	        }
-	    }
-	}
+
 
 }
--- src/org/rhok/offlike/OfflikeFragmentActivity.java
+++ src/org/rhok/offlike/OfflikeFragmentActivity.java
@@ -4,14 +4,20 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
+import android.util.Log;
 
 
 public class OfflikeFragmentActivity extends FragmentActivity {
 
-	PendingLikesSQLHelper pending_likes_sql;
+
+	public final static int REQUEST_ID_SCAN=0;
+
+	private PendingLikesSQLHelper pending_likes_sql;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +58,36 @@ public class OfflikeFragmentActivity extends FragmentActivity {
 			 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			 startActivity(intent);
 			 return true;
+
+	     case R.id.menu_scan:
+	         Intent scan_intent = new Intent("com.google.zxing.client.android.SCAN");
+	         scan_intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+	         startActivityForResult(scan_intent, REQUEST_ID_SCAN);
+	         break;
 		 }	
 	  return super.onOptionsItemSelected(item);
 	 }
 
+	 @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		this.getMenuInflater().inflate(R.menu.menu_omnipresent, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	 
+		
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	    if (requestCode == REQUEST_ID_SCAN) {
+	        if (resultCode == RESULT_OK) {
+	            String contents = intent.getStringExtra("SCAN_RESULT");
+	            //String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	            Log.i("","contents" + contents);
+	            Intent i=new Intent(this,OfflikeActivity.class);
+	            i.setData(Uri.parse(contents));
+		            this.startActivity(i);
+		            // Handle successful scan
+		        } else if (resultCode == RESULT_CANCELED) {
+		            // Handle cancel
+		        }
+	    }
+	}
 }
